911c1b0122b601e542903a3573df9052505ba1a3
eb4s <tb121008@wgcloud.org>
2023-10-16 10:57:27 -0500

    added new steps to readme

--- tut.py
+++ tut.py
@@ -3,5 +3,10 @@ from audioop import add
 
 print("Hello world!")
 
-#add second message
+# add second message
 print("Hola!")
+
+# collect new user's name
+name = input("Enter your name: ")
+
+print("Welcome " + name)
